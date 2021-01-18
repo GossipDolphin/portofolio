38ef92e68d6f2aec73f02c05c514ea8b98866acf
Mmoll <mmollevik@hotmail.com>
2021-01-18 22:32:16 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import characterIdle from './magnusPixelOneX32.gif';
+import characterFlying from './magnusPixelRocketV2.gif';
+import characterFalling from './magnusPixelFalling.gif';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [characterURI, setCharacterUri] = useState(characterIdle);
+
+  const [lastScroll, setLastScroll] = useState(0);
+  useEffect(() => {
+    window.addEventListener("scroll", function () {
+      var offset = window.pageYOffset;
+      if (offset > lastScroll) {
+        setCharacterUri(characterFalling)
+        setLastScroll(window.pageYOffset)
+      } else if (offset < lastScroll) {
+        setCharacterUri(characterFlying)
+        setLastScroll(window.pageYOffset)
+      }
+      setTimeout(function() {
+        setCharacterUri(characterIdle)
+        setLastScroll(window.pageYOffset)
+      }, 500);
+    }
+  ,[])})
+  
+
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <img src={characterURI} className="App-logo" alt="logo" />
+      </div>
     </div>
+    <footer>
+    </footer>
+    </>
   );
 }
 
